8e04b905fc05654e38ea2b193c0482dfac2fa4c3
Michele Puetz <puetz@b-tu.de>
2022-12-22 21:36:56 +0100

    Added test of hard-sphere collision model

--- tests/test_physical_models.cpp
+++ tests/test_physical_models.cpp
@@ -101,7 +101,7 @@ void testFokkerPlanckEquationConstantDrag()
                 );
         };
 
-    auto QmomPtr = Qmom::makeShared("QmomGaG", nMoments, coreInversion,
+    auto QmomPtr = Qmom::makeShared(qmomType, nMoments, coreInversion,
         eigenSolver, nullptr, momentsRateOfChangeFunction);
 
     auto moments = getFpeReferenceMoments(nMoments);
@@ -162,7 +162,7 @@ void testFokkerPlanckEquationVelocityDependentDrag()
                     );
             };
 
-        auto QmomPtr = Qmom::makeShared("QmomGaG", nMoments, coreInversion,
+        auto QmomPtr = Qmom::makeShared(qmomType, nMoments, coreInversion,
             eigenSolver, nullptr, momentsRateOfChangeFunction);
 
         QmomPtr->compute(moments.get());
@@ -184,12 +184,143 @@ void testFokkerPlanckEquationVelocityDependentDrag()
 }
 
 
+void testHardSphereCollision()
+{
+
+    int nMoments = 4;
+
+    std::string qmomType = "QmomStd";
+    double absTol = 1e-9;
+
+    // Shift of NDF; must be on the interval (-1, 1) if Chebyshev moments are
+    // used
+    double shift = 0.4; //0.4;
+
+    // Use normalized and shifted Chebyshev moments as input for simplicity
+    std::shared_ptr<double[]> chebyshevMoments
+    (
+        static_cast<double*>(mkl_malloc(nMoments*sizeof(double), MALLOC_ALIGN)),
+        &mkl_free
+    );
+    for (int k=0; k<nMoments; k++) {
+        chebyshevMoments[k] = getChebyshevMoment(k);
+    }
+    std::shared_ptr<double[]> moments
+    (
+        static_cast<double*>(mkl_malloc(nMoments*sizeof(double), MALLOC_ALIGN)),
+        &mkl_free
+    );
+    linearMomentTransform(shift, 1, nMoments, chebyshevMoments.get(), moments.get());
+    for (int k=0; k<nMoments; k++) {
+        moments[k] /= moments[0];
+    }
+
+    auto coreInversion = CoreInversionAlgorithm::makeShared(
+        CoreInversionAlgorithmFactory::keys()[0],
+        nMoments
+    );
+
+    auto eigenSolver = RealEigenSolver::makeShared(
+        RealEigenSolverFactory::keys()[0],
+        nMoments/2,
+        EigenProblemType::EigenPairs
+    );
+
+    // Test implementation using dynamic selection and default coefficient of
+    // restitution (< 1) -> inelastic collision
+    {
+        auto collisionModelPtr = PhysicalModelFactory::makeShared("HardSphereCollision1D");
+
+        std::function<int(double * const, double * const, int, int, double*)> 
+            momentsRateOfChangeFunction
+            =
+            [&](double * const nodes, double * const weights, int nNodes, 
+                int nMoments, double* momentsRateOfChange) {
+                    return collisionModelPtr->computeMomentsRateOfChange(
+                        nodes, weights, nNodes, nMoments, momentsRateOfChange
+                    );
+            };
+
+        auto QmomPtr = Qmom::makeShared(qmomType, nMoments, coreInversion,
+            eigenSolver, nullptr, momentsRateOfChangeFunction);
+
+        QmomPtr->compute(moments.get());
+
+        auto momentsRateOfChange = QmomPtr->momentsRateOfChange();
+
+        for (int k=0; k<nMoments; k++) {
+            std::printf("%7.6e\n", momentsRateOfChange[k]);
+        }
+
+        // Zeroth moment must remain constant
+        assert(std::abs(momentsRateOfChange[0]) < absTol);
+
+        // The first moment must remain constant due to conservation of momentum
+        assert(std::abs(momentsRateOfChange[1]) < absTol);
+
+        // The second moment (proportional to total kinetic energy) must decrease
+        assert(momentsRateOfChange[2] < 0);
+
+    }
+
+    // Now test implementation with elastic collision
+    {
+        double coefficientOfRestitution = 1;
+
+        std::shared_ptr<HardSphereCollision1D> collisionModelPtr
+        (
+            new HardSphereCollision1D
+            (
+                coefficientOfRestitution,
+                defaultModelConstants::particleDiameter,
+                nMoments
+            )
+        );
+
+        std::function<int(double * const, double * const, int, int, double*)> 
+            momentsRateOfChangeFunction
+            =
+            [&](double * const nodes, double * const weights, int nNodes, 
+                int nMoments, double* momentsRateOfChange) {
+                    return collisionModelPtr->computeMomentsRateOfChange(
+                        nodes, weights, nNodes, nMoments, momentsRateOfChange
+                    );
+            };
+
+        auto QmomPtr = Qmom::makeShared(qmomType, nMoments, coreInversion,
+            eigenSolver, nullptr, momentsRateOfChangeFunction);
+
+        QmomPtr->compute(moments.get());
+
+        auto momentsRateOfChange = QmomPtr->momentsRateOfChange();
+
+        for (int k=0; k<nMoments; k++) {
+            std::printf("%7.6e\n", momentsRateOfChange[k]);
+        }
+
+        // Zeroth moment must remain constant
+        assert(std::abs(momentsRateOfChange[0]) < absTol);
+
+        // The first moment must remain constant due to conservation of momentum
+        assert(std::abs(momentsRateOfChange[1]) < absTol);
+
+        // The second moment (proportional to total kinetic energy) must remain
+        // constant during elastic collisions
+        assert(std::abs(momentsRateOfChange[2]) < absTol);
+
+    }
+
+}
+
+
 int main()
 {
 
     testFokkerPlanckEquationConstantDrag();
 
     testFokkerPlanckEquationVelocityDependentDrag();
+
+    testHardSphereCollision();
 
     return 0;
 }
