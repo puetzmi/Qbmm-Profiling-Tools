b172aa8c5c5ae6d6cb5663dfd13f7ad55c15cd07
Michele Puetz <puetz@b-tu.de>
2022-12-26 13:50:22 +0100

    Prepared general eigensolver benchmark for execution

--- numerical_study/0.1_eigensolve_benchmark_general/01a_eigenvalues_only/run.sh
+++ numerical_study/0.1_eigensolve_benchmark_general/01a_eigenvalues_only/run.sh
@@ -1,22 +1,8 @@
-#!/usr/bin/bash
-# This script runs the general eigensolver benchmark application using the parameters below.
+#!/usr/bin/env bash
+# This script runs the general eigensolver benchmark application using the run-script in the parent directory with `EIGVECS=false`, i.e. computing only eigenvalues.
 # It must be ensured that the current working directory contains the proper executable (or a link to it) and that environment variables such as library paths are set properly.
 
-## PARAMETERS
-NMIN=2                  # minumum matrix size
-NMAX=100                # maximum matrix size
-NMAT=1000               # number of matrices per size
-NEXEC=1000              # number of executions per matrix
-OUTFILE=cpu_times.out   # output filename
-EIGVECS=false           # compute eigenvalues only
 
-
-## DISABLE MULTI-THREADING
-export MKL_DYNAMIC="FALSE"
-export MKL_NUM_THREADS=1
-export OMP_DYNAMIC="FALSE"
-export OMP_NUM_THREADS=1
-
-
-## EXECUTE
-./eigensolver_benchmark_general n_min=$NMIN n_max=$NMAX n_matrices=$NMAT n_exec=$NEXEC outfile=$OUTFILE random_seed=$RANDSEED compute_eigenvectors=$EIGVECS
+export EIGVECS=false            # compute eigenvalues only
+$(dirname $0)/../run.sh         # run case
+unset EIGVECS
